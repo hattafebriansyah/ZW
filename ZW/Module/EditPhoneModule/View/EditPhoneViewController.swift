69aca0a64ad0835d1a2e9d7ae62abfd064e0cb74
aftaarsyad <aftaarsyad@gmail.com>
2022-04-11 00:52:39 -0700

    add phone edit

---
+++ ZW/Module/EditPhoneModule/View/EditPhoneViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EditPhoneViewController.swift
+//  ZW
+//
+//  Created by user215381 on 4/10/22.
+//
+
+import UIKit
+
+class EditPhoneViewController: UIViewController {
+
+    @IBOutlet weak var phoneNoTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnBack(_ sender: Any) {
+    }
+    
+    @IBAction func btnSubmit(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
